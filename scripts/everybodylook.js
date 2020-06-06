131597a2ca00dfd2dd6e958daebfc4c95fbb0bdd
Alex Okafor <okafora@gmail.com>
2020-06-06 06:12:48 -0400

    added "pull all to scene" as a right click option in the scene directory

--- scripts/everybodylook.js
+++ scripts/everybodylook.js
@@ -1,19 +1,28 @@
 
-Hooks.on("getSceneNavigationContext", (html, contextOptions) => {
-    contextOptions.push({
+
+function getContextOption(idField) {
+    return {
         name: "EVERYBODYLOOK.LOOK",
         icon: '<i class="fas fa-directions"></i>',
         condition: li => game.user.isGM,
         callback: li => {
-            let scene = game.scenes.get(li.data("sceneId"));
+            let scene = game.scenes.get(li.data(idField));
             if (!scene.data.active) {
                 scene.activate();
             } else {
-                let scene = game.scenes.get(li.data("sceneId"));
+                let scene = game.scenes.get(li.data(idField));
                 for (let u of game.users) {
                     game.socket.emit('pullToScene', scene.data._id, u.data._id);
                 }
             }
         }
-    });
-})
+    };
+}
+Hooks.on("getSceneNavigationContext", (html, contextOptions) => {
+    contextOptions.push(getContextOption('sceneId'));
+});
+
+Hooks.on("getSceneDirectoryEntryContext", (html, contextOptions) => {
+    console.log('getting context', contextOptions);
+    contextOptions.push(getContextOption('entityId'));
+});
